62079bd8ca593d79dbb2baea135a93d52054bad2
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-13 15:19:18 -0500

    Exerceise 00 complete

---
+++ 00exercise.py
@@ -0,0 +1,34 @@
+fav_colours = ['blue', 'navy', 'black', 'white', 'grey']
+friend_age = [25, 25, 26, 26, 25]
+coin_flip = ['T', 'H','T','T','T']
+fav_artists = ['A7X', 'Metallica', 'Trivium']
+
+
+my_words = {
+  'python': 'a big snake',
+  'cat': 'a fluffy friend',
+  'sheep': 'looks like a cloud'
+}
+
+movies = {
+  'The Shawshand Redemption': 1994,
+  'The Godfather': 1975,
+  'The Dark Knight': 2008
+}
+
+cities = {
+    'Toronto': 6000000,
+    'Montreal': 3000000,
+    'Delhi':42000000
+}
+
+friends = {
+    'Rishabh': 25,
+    'Maria': 26,
+    'Sami': 26,
+    'Harsh': 25
+}
+
+print(movies)
+print(cities)
+print(friends)
