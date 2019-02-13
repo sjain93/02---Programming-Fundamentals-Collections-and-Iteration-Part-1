db8be4f6b0a307932e289ad3e8d6b3c26304c4f1
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-02-13 16:37:36 -0500

    Ex 2

---
+++ 02exercise.py
@@ -0,0 +1,14 @@
+from exercise import *
+
+print(fav_colours[-1])
+
+cities['New York'] = '8623000'
+
+coin_flip.reverse()
+new_coin_flip = list(coin_flip)
+
+print(cities['New York'])
+
+for i in fav_artists:
+    print("I think {} is a great band".format(i))
+    
